92b1eacce784af42bf5e1ee64c6cd97579d97595
IALuir <ALuir2000@outlook.com>
2024-05-06 15:42:01 +0800

    redis地址修改

--- front/src/views/login.vue
+++ front/src/views/login.vue
@@ -76,8 +76,8 @@ const router = useRouter();
 const { proxy } = getCurrentInstance();
 
 const loginForm = ref({
-  username: "admin",
-  password: "admin123",
+  username: "test_admin",
+  password: "123456",
   rememberMe: false,
   code: "",
   uuid: ""
